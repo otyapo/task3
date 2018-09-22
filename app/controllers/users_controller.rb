635a2d6f26925b9681351ba1c8a9222a93299bb7
山田秀明 <yamadahideaki@yamadahideakinoMacBook-Pro.local>
2018-09-22 19:23:09 +0900

    [finish]921

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+
+  def index
+
+
+  end
+
+  def show
+  	@user = User.find(params[:id])
+
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+  	user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to books_path(book.id)
+    end
+
+private
+    def user_params
+    	params.require(:user).permit(:name, :profile_image)
+    end
+end
